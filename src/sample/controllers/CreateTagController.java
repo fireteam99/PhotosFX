11bb934b11976a73da476884bda24310b1995031
Robert Cheng <rjc345@scarletmail.rutgers.edu>
2020-03-18 14:05:37 -0400

    created skeleton code for controllers

---
+++ src/sample/controllers/CreateTagController.java
@@ -0,0 +1,41 @@
+package sample.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CreateTagController {
+
+    @FXML
+    private TextField keyField;
+
+    @FXML
+    private TextField valueField;
+
+    @FXML
+    private Button submitNewTagButton;
+
+    @FXML
+    private Button cancelNewTagButton;
+
+    @FXML
+    private Button closeCreateTagButton;
+
+    @FXML
+    void cancelNewTag(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closeCreateEdit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submitNewTag(ActionEvent event) {
+
+    }
+
+}
+
---
+++ src/sample/controllers/CreateUserController.java
@@ -0,0 +1,40 @@
+package sample.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CreateUserController {
+
+    @FXML
+    private TextField newUserBox;
+
+    @FXML
+    private TextField newPasswordBox;
+
+    @FXML
+    private Button submitNewUserButton;
+
+    @FXML
+    private Button cancelNewUserButton;
+
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    void cancelCreateNewUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closeCreateNewUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submitNewUser(ActionEvent event) {
+
+    }
+
+}
