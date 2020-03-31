d470850bafc260729f75e194e9a3f5dfdec97f5e
fireteam99 <raymancheungsy@gmail.com>
2020-03-31 01:28:04 -0400

    got basic skeleton running

--- src/main/java/com/photos/Main.java
+++ src/main/java/com/photos/Main.java
@@ -10,12 +10,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/admin.fxml"));
         primaryStage.setTitle("Photos21");
         primaryStage.setScene(new Scene(root, 1000, 650));
         primaryStage.show();
-        primaryStage.setMinWidth(700);
-        primaryStage.setMinHeight(400);
+        primaryStage.setMinWidth(800);
+        primaryStage.setMinHeight(550);
 
         UserList test = new UserList();
         test.setAdmin();
