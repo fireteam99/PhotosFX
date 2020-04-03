a47774907851158632a3fef371d874ade4d48b75
Robert Cheng <rjc345@scarletmail.rutgers.edu>
2020-04-03 00:00:19 -0400

    fixed serialization issue

--- src/main/java/com/photos/Main.java
+++ src/main/java/com/photos/Main.java
@@ -1,10 +1,13 @@
 package com.photos;
+import com.photos.controllers.User;
 import com.photos.controllers.UserList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class Main extends Application {
 
@@ -17,8 +20,16 @@ public class Main extends Application {
         primaryStage.setMinWidth(700);
         primaryStage.setMinHeight(400);
 
+        //testing and debugging -- IGNORE
         UserList test = new UserList();
         test.setAdmin();
+        User bob = new User("bob", "pass");
+        test.addUser(bob);
+        test.writeToSerFile(test);
+        test.deserialize();
+
+        System.out.println("List of users...");
+        test.printUserList();
 
     }
 
