edeecdd2850ca3840cdda2c796331366ed86a6de
fireteam99 <raymancheungsy@gmail.com>
2020-04-04 18:53:10 -0400

    changed thing in main

--- src/main/java/com/photos/Main.java
+++ src/main/java/com/photos/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 //        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
 //        Parent root = FXMLLoader.load(getClass().getResource("/fxml/admin.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/admin.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
 
         primaryStage.setTitle("Photos21");
         primaryStage.setScene(new Scene(root, 1000, 650));
