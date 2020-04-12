23abc8d208f2bc7f61b184a23133badf80e3e298
Robert Cheng <rjc345@scarletmail.rutgers.edu>
2020-04-12 00:41:14 -0400

    Added and implemented album class

--- src/main/java/com/photos/controllers/User.java
+++ src/main/java/com/photos/controllers/User.java
@@ -1,16 +1,24 @@
 package com.photos.controllers;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
+/*
+    When a user wants to create a new album, they must create a new array list of Picture objects first,
+    then add it to their collection of albums (array list). Then this array list of albums must be
+    serialized, via the AlbumsList class, to the master HashMap of albums.
+ */
 public class User implements Serializable{
 
     private String username = "";
     private String password = "";
+    private ArrayList<Album> albums; //collection of albums for this user
 
     //constructor to create new user objects
     public User(String user, String pw) {
         this.username = user;
         this.password = pw;
+        this.albums = new ArrayList<Album>();
     }
 
     public String getUsername(){
@@ -19,6 +27,10 @@ public class User implements Serializable{
 
     public String getPassword(){
         return this.password;
+    }
+
+    public ArrayList<Album> getAlbums(){
+        return this.albums;
     }
 
 }
