4c7f4855b42d795ca01f9ebc31ab4a9eee055545
fireteam99 <raymancheungsy@gmail.com>
2020-04-12 13:13:52 -0400

    removed extra modal

---
+++ src/main/java/com/photos/controllers/SingleInputModalController.java
@@ -0,0 +1,10 @@
+package com.photos.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SingleInputModalController {
+    @FXML
+    private Button closeButton;
+
+}
